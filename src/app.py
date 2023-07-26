ca35693b735a552c2f92950f18925d1da65b90a1
karinaR <decodikiaro@gmail.com>
2023-07-26 14:00:39 -0300

    creo instancia flask

--- src/app.py
+++ src/app.py
@@ -1,3 +1,10 @@
 from flask import Flask
-print ('hola mundo')
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+if __name__=='master':
+ app.run(debug=True)
 
